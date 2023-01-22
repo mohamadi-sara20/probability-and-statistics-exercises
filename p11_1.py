5061ac6ca03fb97135a9c944c6320092b15335d3
mohamadisara20 <mohamadi.sara20@gmail.com>
2023-01-22 14:16:22 +0100

    add gitignore + p2

---
+++ p11_1.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def estimate_slope(x, mean_x, y, mean_y):
+    b1 = 0
+    den = 0
+    for i in range(len(x)):
+        b1 += (x[i] - mean_x) * (y[i] - mean_y)
+        den += (x[i] - mean_x) ** 2
+    
+    return b1 / den
+
+def estimate_intercept(mean_y, b1, mean_x):
+    return mean_y - (b1 * mean_x)
+
+def calculate_residuals(y, y_pred):
+    return np.subtract(y, y_pred)
+   
+
+if __name__ == "__main__":
+    x = [17.3, 19.3, 19.5, 19.7, 22.9, 23.1, 26.4, 26.8, 27.6, 28.1, 28.2, 28.7, 29.0, 29.6, 29.9, 29.9, 30.3, 31.3, 36.0, 39.5, 40.4, 44.3, 44.6, 50.4, 55.9]
+    y = [71.7, 48.3, 88.3, 75.0, 91.7, 100.0, 73.3, 65.0, 75.0, 88.3, 68.3, 96.7, 76.7, 78.3, 60.0, 71.7, 85.0, 85.0, 88.3, 100.0, 100.0, 100.0, 91.7, 100.0, 71.7]
+
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    b1 = estimate_slope(x=x,y=y,mean_x=mean_x, mean_y=mean_y)
+    b0 = estimate_intercept(mean_y=mean_y, b1=b1, mean_x=mean_x)
+
+    # (a)
+    print(f"B1: {b1} and B0: {b0}")
+
+    # (b)
+    pred = b1 * 30 + b0
+    print(f"μY|30: {pred}")
+
+    # (c)     
+    y_pred = []
+    for i in range(len(x)):
+        y_pred.append(b1 * x[i] + b0)
+
+    residuals = calculate_residuals(y=y, y_pred=y_pred)
+    plt.scatter(x, residuals)
+    plt.axhline(y=0, color='r', linestyle='-')
+    plt.show()
---
+++ p11_2.py
@@ -0,0 +1,17 @@
+from p11_1 import *
+import numpy as np
+
+if __name__ == "__main__":
+    x = [77,50,71,72,81,94,96,99,67]
+    y = [82,66,78,34,47,85,99,99,68]
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    b1 = estimate_slope(x=x, mean_x=mean_x, y=y, mean_y=mean_y)
+    b0 = estimate_intercept(b1=b1, mean_x=mean_x, mean_y=mean_y)
+    # (a)
+    print(f"B1: {b1} and B0: {b0}")
+    # (b)
+    pred = b1 * 85 + b0
+    print(f"μY|85: {pred}")
+
+    
