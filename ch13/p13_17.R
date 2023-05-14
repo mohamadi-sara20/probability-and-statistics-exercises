7d64b52244c616aaba441b453a0abb936e8f476d
sara <mohamadi.sara20@gmail.com>
2023-05-14 19:04:22 +0200

    add q17

---
+++ ch13/p13_17.R
@@ -0,0 +1,18 @@
+library(DescTools)
+
+data <- data.frame(
+    sampling_method = c("D", "D", "D", "D", 
+    "M", "M", "M", "M",
+    "S", "S", "S", "S", 
+    "SRK", "SRK", "SRK", "SRK", 
+    "K", "K", "K", "K"),
+    y = c(85, 55, 40, 77,
+        75, 45, 35, 67,
+        31, 20, 9, 37,
+        43, 21, 15, 27,
+        17, 10, 8, 15 ))
+
+data$sampling_method <- as.factor(data$sampling_method)
+model <- aov(y ~ sampling_method, data = data)
+summary(anv)
+TukeyHSD(model, which = 'sampling_method')
