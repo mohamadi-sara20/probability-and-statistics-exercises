47340b4f9c19f7c30d9a6ec3777fae3331209404
sara <mohamadi.sara20@gmail.com>
2023-06-08 23:09:32 +0200

    add q32

---
+++ ch14/p14_32.R
@@ -0,0 +1,29 @@
+library(partR2)
+library(lme4)
+library(lmerTest)
+
+cereal     <- rep(c("1", "1", "2", "2", "3", "3"), n=6)
+power <-  rep(c("Low", "Current", "High"), each=12)
+
+yield  <- c(
+            395, 390, 392, 392, 402, 405,
+            401, 400, 394, 401, 399, 399, 
+            396, 399, 390, 392, 404, 403, 
+            400, 402, 395, 502, 400, 399, 
+            410, 408, 404, 406, 415, 412, 
+            408, 407, 401, 400, 413, 415
+)
+
+data <- data.frame(power, cereal, yield)
+
+m <- lmer(yield ~ power + (1|cereal) + (1|cereal:power), 
+data=data)
+summary(m)
+anova(m)
+
+
+
+# interaction.plot(x.factor = data$power, 
+#                  trace.factor = data$cereal,
+#                  response = data$yield)
+
