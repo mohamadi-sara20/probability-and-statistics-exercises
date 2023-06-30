64af653193e8306b5459d455369c32cdcc418971
sara <mohamadi.sara20@gmail.com>
2023-06-30 15:24:09 +0200

    update q36

--- ch16/p16_36.R
+++ ch16/p16_36.R
@@ -1,7 +1,7 @@
 library(DescTools)
 
-midterm <- c(6, 9, 2, 8, 5, 1, 7, 4, 3)
-final <- c(480, 395, 575, 550, 510, 545, 400, 465, 420)
+panel <- c(6, 9, 2, 8, 5, 1, 7, 4, 3)
+price <- c(480, 395, 575, 550, 510, 545, 400, 465, 420)
 # Is there a significance correlation?
-cor <- cor.test(x = midterm, y = final, method = "spearman", exact = FALSE)
+cor <- cor.test(x = panel, y = price, method = "spearman", exact = FALSE)
 print(cor$p.value)
